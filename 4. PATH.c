bc1ab91fd52e842761cdf4ff2110dda3a3e08fd1
MINS2405 <9963@holbertonstudents.com>
2025-01-06 14:00:25 +0100

    simple shell

--- 4. PATH.c
+++ 4. PATH.c
@@ -2,38 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct dir_node {
-    char *dir;
-    struct dir_node *next;
-};
+struct dir_node
+{
+char *dir;
+struct dir_node *next;
+}
 
 struct dir_node *build_path_list(void)
 {
 char *path = _getenv("PATH");
 char *token;
-struct dir_node *head = NULL, *current = NULL, *new_node;
-    
+struct dir_node *head = NULL, *current = NULL, *new_node;  
 if (path == NULL)
-return (NULL);
-    
+return (NULL); 
 token = strtok(path, ":");
 while (token != NULL)
 {
 new_node = malloc(sizeof(struct dir_node));
 if (new_node == NULL)
 return (NULL);
-
 new_node->dir = strdup(token);
-new_node->next = NULL;
-        
+new_node->next = NULL;   
 if (head == NULL)
 head = new_node;
 else
-current->next = new_node;
-        
+current->next = new_node;       
 current = new_node;
 token = strtok(NULL, ":");
-}
-    
+}   
 return (head);
 }
---
+++ README.md
@@ -0,0 +1,38 @@
+Simple Shell
+A basic UNIX command interpreter written in C
+
+>Description
+This project is a simple implementation of a UNIX shell. It allows the execution of basic commands and offers features such as process management and basic input/output redirection
+
+>Features
+<Execution of simple commands
+<Path handling (PATH)
+<Child process management
+<Basic input/output redirection
+<Signal handling (Ctrl+C)
+
+>Compilation
+To compile the shell, use the following command
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -o hsh
+
+>Usage
+Launch the shell in interactive mode
+./hsh
+
+Or use it in non-interactive mode
+echo "/bin/ls" | ./hsh
+
+>Examples
+$ ./hsh
+($) /bin/ls
+hsh main.c shell.c
+($) exit
+$
+
+>Authors:
+MINS2405
+ni-mdj
+
+>License
+This project is licensed under the MIT License. See the LICENSE file for details
+
